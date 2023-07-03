87fdd03b63b2d7779e766f0845498ea0d115ccb5
SYERAA007 <simhasimha4321.com>
2023-07-03 21:41:48 +0530

    first commmit

--- Forms/views.py
+++ Forms/views.py
@@ -7,7 +7,6 @@ def form_data(request):
     if request.method=='POST':
         username=request.POST['un']
         password=request.POST['pswd']
-        # request.POST={'username':'un','password':'pswd'}
         Form.objects.get_or_create(UserName=username,PassWord=password)[0].save()
         return HttpResponse('Data is inserted successfully',request.POST)
     return render(request,'form_data.html')
@@ -20,26 +19,26 @@ def form_topic(request):
     return render(request,'form_topic.html')
 
 def form_webpage(request):
+    LTO=Topic.objects.all()
+    d={'LTO':LTO}
     if request.method=='POST':
         topic=request.POST['topic'] 
-        Name=request.POST['names']
-        Url=request.POST['urls']
+        Name=request.POST.get('names')
+        Url=request.POST.get('urls')
         TO=Topic.objects.get(topic_name=topic)
-        # request.POST={'topic':TO,'Name':'names','Url':'urls'}
         Webpage.objects.get_or_create(topic_name=TO,name=Name,url=Url)[0].save()
         return HttpResponse('Webpage added successfully')
-    return render(request,'form_webpage.html')
+    return render(request,'form_webpage.html',d)
 
 def form_record(request):
+    LTO=Topic.objects.all()
+    LWO=Webpage.objects.all()
+    d={'LTO':LTO,'LWO':LWO}
     if request.method=='POST':
-        topic=request.POST['topic'] 
-        Name=request.POST['names']
-        Date=request.POST['date']
-        Author=request.POST['author']
-        TO=Topic.objects.get(topic_name=topic)
+        Name=request.POST.get('names')
+        Date=request.POST.get('date')
+        Author=request.POST.get('author')
         NO=Webpage.objects.get(name=Name)
-        # request.POST={'Name':NO,'Date':'date','Author':'author'}
         AccessRecord.objects.get_or_create(name=NO,date=Date,author=Author)[0].save()
-        
         return HttpResponse('Record added successfully')
-    return render(request,'form_record.html')
+    return render(request,'form_record.html',d)
